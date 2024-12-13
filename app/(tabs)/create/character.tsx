ba57c53cacca5dcb2604bc9726ad3c2e5b7f2566
Connor Mattless <connor.mattless@ucd.ie>
2024-12-13 17:07:30 +0000

    feat: Add character creation functionality

---
+++ app/(tabs)/create/character.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { View, Button, Image, Text } from "react-native";
+import StepForm from "@/components/Forms/StepForm";
+import { useRouter } from "expo-router";
+import useRequests from "@/hooks/useRequests";
+import { useSession } from "@/contexts/AuthContext";
+import { useToast } from "@/contexts/ToastContext";
+
+const Character = () => {
+	const router = useRouter();
+	const { session, user } = useSession();
+	const { loading, error, sendRequest } = useRequests();
+	const showToast = useToast();
+
+	const [factions, setFactions] = React.useState([]);
+
+	React.useEffect(async () => {
+		await sendRequest({ endpoint: "/factions", method: "GET" }).then((res) => {
+			res.map((faction) => {
+				setFactions((prev) => [
+					...prev,
+					{ label: faction.name, value: faction._id },
+				]);
+			});
+		});
+	}, [user]);
+
+	const stepsConfig = [
+		{
+			type: "text",
+			name: "name",
+			label: "Character Name",
+			placeholder: "Enter character name",
+			required: true,
+		},
+		{
+			type: "textarea",
+			name: "description",
+			label: "Character Description",
+			placeholder: "Describe your character",
+			required: true,
+		},
+		{
+			type: "text",
+			name: "profession",
+			label: "Character Profession",
+			placeholder: "Enter character profession",
+			required: true,
+		},
+		...(factions.length > 0
+			? [
+					{
+						type: "dropdown",
+						name: "faction",
+						label: "Character Faction",
+						required: true,
+						options: factions,
+					},
+			  ]
+			: []),
+	];
+
+	const handleFormComplete = async (formData) => {
+		formData["year"] = parseInt(formData["year"]);
+		console.log(`Bearer ${session}`);
+		await sendRequest({
+			endpoint: "/characters",
+			method: "POST",
+			data: formData,
+			headers: { authorization: `Bearer ${session}` },
+		});
+		showToast("Character created successfully", 3000);
+		router.push("/main");
+	};
+
+	return (
+		<View className="flex-1 bg-[#333333]">
+			<View className="flex-1 justify-center items-center">
+				<Image
+					style={{ width: "100%", height: "55%" }}
+					source={require("@/assets/images/create_world_full.png")}
+					resizeMode="cover"
+				/>
+				<Image
+					className="self-center"
+					source={require("@/assets/images/LF_Trans.png")}
+					style={{ width: 80, height: 122 }}
+				/>
+				<Text className="text-white text-xl font-bold mb-4">
+					Create a Character
+				</Text>
+			</View>
+			<View className="flex-1 items-center">
+				<StepForm steps={stepsConfig} onComplete={handleFormComplete} />
+			</View>
+		</View>
+	);
+};
+
+export default Character;
